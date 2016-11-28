7527bb740b94a1e8bdefd3d34b63fc997b7fc163
Lindsay Fenwick <lindsayafenwick@gmail.com>
2016-11-28 11:42:22 -0800

    changed images and fade in

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -13,50 +13,67 @@ beforeOpen: function lightbox_open() {
   document.getElementById('light').style.display = 'block';
   document.getElementById('fade').style.display = 'block';
   lightBoxVideo.play();
+  lightBoxVideo.addEventListener('ended',lightbox_close,false);
 }
-
-function closeMe(){
-        t = setTimeout("self.close()",3000);
-    }
 
 function lightbox_close() {
   var lightBoxVideo = document.getElementById("video");
-  document.getElementById('light').style.display = 'none';
-  document.getElementById('fade').style.display = 'none';
+  fadeOut(document.getElementById('light'));
+  fadeOut(document.getElementById('fade'));
   lightBoxVideo.pause();
-  video.currentTime = 0;
+  lightBoxVideo.currentTime = 0;
+}
+
+function fadeOut(el){
+  el.style.opacity = 1;
+
+  (function fade() {
+    if ((el.style.opacity -= .1) < 0) {
+      el.style.display = "none";
+    } else {
+      requestAnimationFrame(fade);
+    }
+  })();
 }
 
 
+var timeoutID;
+ 
+function setup() {
+    this.addEventListener("mousemove", resetTimer, false);
+    this.addEventListener("mousedown", resetTimer, false);
+    this.addEventListener("keypress", resetTimer, false);
+    this.addEventListener("DOMMouseScroll", resetTimer, false);
+    this.addEventListener("mousewheel", resetTimer, false);
+    this.addEventListener("touchmove", resetTimer, false);
+    this.addEventListener("MSPointerMove", resetTimer, false);
+ 
+    startTimer();
+}
+setup();
+ 
+function startTimer() {
+    // wait 2 seconds before calling goInactive
+    timeoutID = window.setTimeout(goInactive, 50000);
+}
+ 
+function resetTimer(e) {
+    window.clearTimeout(timeoutID);
+ 
+    goActive();
+}
+ 
+function goInactive() {
+    lightbox_close();
+    window.location.href="page3.html";
 
-
-////////////////////////////////////////
-// Our Script 
-////////////////////////////////////////
-$(document).ready(function(){
-  // Initiate FitVid.js
-  $(".video-container").fitVids();
-
-  // Iframe/player variables
-  var iframe = $('#video')[0];
-  var player = $f(iframe);
-
-  // Open on play
-  $('.play').click(function(){
-    $('.overlay').css('left', 0)
-    $('.overlay').addClass('show')
-    player.api("play");
-  })
-
-  // Closes on click outside
-  $('.overlay').click(function(){
-    $('.overlay').removeClass('show')
-    setTimeout(function() {
-      $('.overlay').css('left', '-100%')
-    }, 300);
-    player.api("pause");
-  })
-});
+}
+ 
+function goActive() {
+    // do something
+         
+    startTimer();
+}
 
 
 
